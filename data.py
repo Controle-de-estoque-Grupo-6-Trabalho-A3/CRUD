7c5a3331c914f48bde19f3ff41ea013ce7452cf3
Rodolfo Ritz <rodolfo.ritza@gmail.com>
2023-10-05 10:33:49 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,33 +1,10 @@
-lista_clientes = [
+lista_alunos = [
     {
-    "id": 1,
-    "nome": "Tonhão do açougue",
-    "altura": 1.5,
-    "peso": 90,
-    "avaliações": [
-      {
-        "data": "01/02/2023",
-        "imc": 40,
-        "classificação": "OBESIDADE GRAVE III"
-      }
-    ]
-  },
-  {
-    "id": 2,
-    "nome": "Renato2",
-    "altura": 1.75,
-    "peso": 85,
-    "avaliações": [
-      {
-        "data": "01/02/2023",
-        "imc": 31.03,
-        "classificação": "OBESIDADE II"
-      },
-      {
-        "data": "01/02/2023",
-        "imc": 27.76,
-        "classificação": "SOBREPESO I"
-      }
-    ]
-  }
+        'id': 1,
+        'nome': 'Renato'
+    },
+    {
+        'id': 2,
+        'nome': 'Luciana'
+    },
 ]
