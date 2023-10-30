b84ff4cbed21b72e0119601cdb51142114f7b4b6
Rodolfo Ritz <rodolfo.ritza@gmail.com>
2023-10-30 18:43:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
-from fastapi import FastAPI
-from data import lista_alunos
-# Itens para exemplificar async:
-# async def get(db: Any = Depends(fake_db)):
-# from fastapi import Depends
-# from apiTools import fake_db
-# from typing import Any
+from fastapi import FastAPI, HTTPException, status, Path
+from apiTools import *
+from data import lista_produtos
 
-app = FastAPI()
+app = FastAPI(    
+   title='Api Depósito de Tintas',
+   version='0.0.1',
+   description="Api com nome das tintas, suas respectivas características e outros itens do depósito.",
+)
 
-@app.get('/get-alunos')
-async def get():
-    return lista_alunos
+@app.get('/produtos',
+        status_code=status.HTTP_200_OK,
+        description='Retorna todos os produtos ou uma lista vazia.',
+        summary='Retorna todos os clientes.'
+        )
+async def get_produtos():
+    try:
+        return lista_produtos
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail='Produtos não encontrados.'
+    )
 
-@app.get('/get-aluno/{id}')
-async def get_by_id(id: int):
-    id_int = int(id) - 1
-    return lista_alunos[id_int]
+@app.get('/produto/{id}', status_code=status.HTTP_200_OK)
+async def get_produto_id(id: int = Path(
+    title='ID do produto',
+    description=f'Deve ser de 1 a {len(lista_produtos)}',
+    gt=1,
+    lt=len(lista_produtos) + 1)
+    ):
+    try:
+        resposta = get_by_id(id, lista_produtos)
+        return resposta
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail='Produto não encontrado.'
+    )
 
-@app.post('/post-aluno')
-async def post(nome: str):
-    new_id = len(lista_alunos) + 1
-    lista_alunos.append({
-        'id': new_id,
-        'nome': nome
-    })
-    return lista_alunos
+@app.post('/produto-create', status_code=status.HTTP_201_CREATED)
+async def create_produto(nome: str, cor: str, lavavel: bool):
+    try:
+        resposta = create_produto_new(nome, cor, lavavel, lista_produtos)
+        return resposta
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail='Produto não criado.'
+    )   
 
-@app.put('/update-aluno/{id}')
-async def update(id: int, nome:str):
-    id_int = int(id) -1
-    aluno_update = lista_alunos[id_int]
-    aluno_update['nome'] = nome
-    return lista_alunos
+@app.put('/produto-update/{id}', status_code=status.HTTP_202_ACCEPTED)
+async def update_produto(nome: str, cor: str, lavavel: bool, id: int):
+    try:
+        resposta = update_produto_by_id(nome, cor, lavavel, lista_produtos, id)
+        return resposta
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Produto com o id:{id} não encontrado'
+    )
 
-@app.delete('/delete-aluno/{id}')
-async def delete(id: int):
-    id_int = int(id)
-    lista_alunos.pop(id_int-1)
-
-    news_ids = 0
-    for aluno in lista_alunos:
-        news_ids += 1
-        aluno['id'] = news_ids
-
-    return lista_alunos
+@app.delete('/produto-delete/{id}', status_code=status.HTTP_200_OK)
+async def delete_produto(id: int):
+    try:        
+        resposta = delete_produto_by_id(lista_produtos, id)
+        return resposta
+    except:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Produtos não encontrado com o id:{id}'
+    )
