d6f608fc624c3b15e78abb39e01987e33fd91519
Rodolfo Ritz <rodolfo.ritza@gmail.com>
2023-10-30 18:42:45 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,14 @@
-lista_alunos = [
+lista_produtos = [
     {
-        'id': 1,
-        'nome': 'Renato'
-    },
-    {
-        'id': 2,
-        'nome': 'Luciana'
-    },
+    "id": 1,
+    "nome": "Tinta Eucalipto",
+    "cor": 'marron eucalipto',
+    "lavavel": True
+  },
+  {
+    "id": 2,
+    "nome": "Tinta branco fosco",
+    "cor": 'branco fosco',
+    "lavavel": False
+  }
 ]
